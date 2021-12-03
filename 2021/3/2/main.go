715098c46c15189827941d2cfa0771847c9537ec
Thomas Döring <thomas.doering@check24.de>
2021-12-03 14:17:37 +0100

    DAY 3 Finish

--- 2021/3/2/main.go
+++ 2021/3/2/main.go
@@ -14,6 +14,7 @@ func main() {
   // STDOUT MUST BE FLUSHED MANUALLY!!!
   defer writer.Flush()
   inputs := readFile("../input.txt")
+
   response := transform(inputs)
   println(strconv.Itoa(int(response)))
 }
@@ -35,10 +36,63 @@ func readFile(filename string) []string {
 }
 
 func transform(inputs []string) int64 {
+	return oxygen(inputs) * co2(inputs)
+}
+
+func oxygen(inputs []string) int64 {
+	i := 0
+
+	for len(inputs) > 1 {
+		binaryNumbers := getBinaryNumbers(inputs)
+		result := computeMostHit(binaryNumbers)
+
+		oc := result[0][i]
+
+		var inputs2 []string
+		for j := 0; j < len(inputs); j++ {
+			if(inputs[j][i] == oc) {
+				inputs2 = append(inputs2, inputs[j])
+			}
+		}
+
+		i++
+		inputs = inputs2
+	}
+
+	return binaryToInt(inputs[0])
+}
+
+func co2(inputs []string) int64 {
+	i := 0
+
+	for len(inputs) > 1 {
+		binaryNumbers := getBinaryNumbers(inputs)
+		result := computeMostHit(binaryNumbers)
+
+		oc := result[1][i]
+
+		var inputs2 []string
+		for j := 0; j < len(inputs); j++ {
+			if(inputs[j][i] == oc) {
+				inputs2 = append(inputs2, inputs[j])
+			}
+		}
+
+		i++
+		inputs = inputs2
+	}
+
+	return binaryToInt(inputs[0])
+}
+
+
+func remove(s []string, i int) []string {
+    s[i] = s[len(s)-1]
+    return s[:len(s)-1]
+}
+
+func computeMostHit(binaryNumbers []string) []string {
 	result := make([]string, 2)
-
-	binaryNumbers := getBinaryNumbers(inputs)
-
 	for i := 0; i < len(binaryNumbers); i++ {
 		c0, c1 := 0,0
 		chars := []rune(binaryNumbers[i])
@@ -49,7 +103,7 @@ func transform(inputs []string) int64 {
 			}
 		}
 
-		if(c1 > c0) {
+		if(c1 >= c0) {
 			result[0] += "1"
 			result[1] += "0"
 		} else {
@@ -58,7 +112,7 @@ func transform(inputs []string) int64 {
 		}
 	}
 
-	return binaryToInt(result[0]) * binaryToInt(result[1])
+	return result
 }
 
 func getBinaryNumbers(inputs []string) []string {
