4e7418f34f6e6a2bdff45e25c8772e0bda5f91ed
Marlux <marlux.td@googlemail.com>
2021-12-15 21:11:52 +0100

    Formating

--- 2021/15/1/main.go
+++ 2021/15/1/main.go
@@ -12,13 +12,13 @@ import (
 )
 
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
 func println(f string) { fmt.Fprintln(writer, f) }
 
 var lines []string
 var result int = 0
 
-var problem [][] int
-
+var problem [][]int
 
 func main() {
 	// STDOUT MUST BE FLUSHED MANUALLY!!!
@@ -33,16 +33,16 @@ func main() {
 func readInput() {
 	i, err := aocutil.NewInputFromFile("session_id")
 	if err != nil {
-	log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	lines, err = i.Strings(2021, 15)
 	if err != nil {
-	log.Fatal(err)
+		log.Fatal(err)
 	}
 }
 
-func stringLinesTo2dIntArray () {
+func stringLinesTo2dIntArray() {
 	problem = make([][]int, len(lines))
 
 	for i, line := range lines {
@@ -59,30 +59,30 @@ func solve() int {
 
 	for i, row := range problem {
 		for j := range row {
-			graph.AddVertex(i * len(row) + j)
+			graph.AddVertex(i*len(row) + j)
 		}
 	}
 
 	for i, row := range problem {
 		for j, value := range row {
-			index := i * len(row) + j
+			index := i*len(row) + j
 			if checkLegitPoint(i-1, j) {
-				graph.AddArc((i-1) * len(row) + j, index,  int64(value))
+				graph.AddArc((i-1)*len(row)+j, index, int64(value))
 			}
 			if checkLegitPoint(i+1, j) {
-				graph.AddArc((i+1) * len(row) + j, index,  int64(value))
+				graph.AddArc((i+1)*len(row)+j, index, int64(value))
 			}
 			if checkLegitPoint(i, j-1) {
-				graph.AddArc((i) * len(row) + j-1, index,  int64(value))
+				graph.AddArc((i)*len(row)+j-1, index, int64(value))
 			}
 			if checkLegitPoint(i, j+1) {
-				graph.AddArc((i) * len(row) + j+1, index,  int64(value))
+				graph.AddArc((i)*len(row)+j+1, index, int64(value))
 			}
 		}
 	}
 
-	best, err := graph.Shortest(0, len(problem) * len(problem[0]) - 1)
-	if err!=nil{
+	best, err := graph.Shortest(0, len(problem)*len(problem[0])-1)
+	if err != nil {
 		log.Fatal(err)
 	}
 
--- 2021/15/2/main.go
+++ 2021/15/2/main.go
@@ -12,13 +12,13 @@ import (
 )
 
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
 func println(f string) { fmt.Fprintln(writer, f) }
 
 var lines []string
 var result int = 0
 
-var problem [][] int
-
+var problem [][]int
 
 func main() {
 	// STDOUT MUST BE FLUSHED MANUALLY!!!
@@ -33,16 +33,16 @@ func main() {
 func readInput() {
 	i, err := aocutil.NewInputFromFile("session_id")
 	if err != nil {
-	log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	lines, err = i.Strings(2021, 15)
 	if err != nil {
-	log.Fatal(err)
+		log.Fatal(err)
 	}
 }
 
-func stringLinesTo2dIntArray () {
+func stringLinesTo2dIntArray() {
 	problem = make([][]int, 5*len(lines))
 
 	xLen := len(lines)
@@ -50,21 +50,21 @@ func stringLinesTo2dIntArray () {
 
 	for i, line := range lines {
 
-		for x:=0; x < 5; x++ {
-			problem[i + (xLen*x)] = make([]int, 5*len(line))
+		for x := 0; x < 5; x++ {
+			problem[i+(xLen*x)] = make([]int, 5*len(line))
 		}
 
 		for j, char := range line {
 			number := int(char) - 48
 
-			for x:= 0; x < 5; x++ {
+			for x := 0; x < 5; x++ {
 				for y := 0; y < 5; y++ {
 					n := number + x + y
-					if(n > 9) {
+					if n > 9 {
 						n = n - 9
 					}
 
-					problem[i+(xLen * x)][j+(yLen * y)] = n
+					problem[i+(xLen*x)][j+(yLen*y)] = n
 				}
 			}
 		}
@@ -76,30 +76,30 @@ func solve() int {
 
 	for i, row := range problem {
 		for j := range row {
-			graph.AddVertex(i * len(row) + j)
+			graph.AddVertex(i*len(row) + j)
 		}
 	}
 
 	for i, row := range problem {
 		for j, value := range row {
-			index := i * len(row) + j
+			index := i*len(row) + j
 			if checkLegitPoint(i-1, j) {
-				graph.AddArc((i-1) * len(row) + j, index,  int64(value))
+				graph.AddArc((i-1)*len(row)+j, index, int64(value))
 			}
 			if checkLegitPoint(i+1, j) {
-				graph.AddArc((i+1) * len(row) + j, index,  int64(value))
+				graph.AddArc((i+1)*len(row)+j, index, int64(value))
 			}
 			if checkLegitPoint(i, j-1) {
-				graph.AddArc((i) * len(row) + j-1, index,  int64(value))
+				graph.AddArc((i)*len(row)+j-1, index, int64(value))
 			}
 			if checkLegitPoint(i, j+1) {
-				graph.AddArc((i) * len(row) + j+1, index,  int64(value))
+				graph.AddArc((i)*len(row)+j+1, index, int64(value))
 			}
 		}
 	}
 
-	best, err := graph.Shortest(0, len(problem) * len(problem[0]) - 1)
-	if err!=nil{
+	best, err := graph.Shortest(0, len(problem)*len(problem[0])-1)
+	if err != nil {
 		log.Fatal(err)
 	}
 
