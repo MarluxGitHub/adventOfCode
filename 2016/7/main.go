57d7dd29cbf9e13919d56c244f6c76a60606afff
Thomas Döring <thomas.doering@check24.de>
2023-11-27 10:14:19 +0100

    FIx 2016 / 7

--- 2016/7/main.go
+++ 2016/7/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"MarluxGitHub/adventOfCode/pkg/strings"
 	"bufio"
 	"fmt"
 	"log"
@@ -12,75 +11,132 @@ import (
 )
 
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
 func println(f string) { fmt.Fprintln(writer, f) }
-func printf(f string) { fmt.Fprintf(writer, f) }
+func printf(f string)  { fmt.Fprintf(writer, f) }
 
 var lines []string
 var result int
 var year = 2016
 var day = 7
 
-
 func main() {
-  	// STDOUT MUST BE FLUSHED MANUALLY!!!
-  	defer writer.Flush()
+	// STDOUT MUST BE FLUSHED MANUALLY!!!
+	defer writer.Flush()
 
-  	readInput()
-
-	result = 0
-  	Solve1()
-  	println("1:" + strconv.Itoa(result))
+	readInput()
 
 	result = 0
-  	Solve2()
-  	println("2:" + strconv.Itoa(result))
+	Solve1()
+	println("1:" + strconv.Itoa(result))
+
+	result = 0
+	Solve2()
+	println("2:" + strconv.Itoa(result))
 }
 
-// Solve part 1
 func Solve1() {
-	for _,line := range lines {
-		a,b,c := getStringsOfLine(line)
+	for _, line := range lines {
+		outer, inner := parseInput(line)
 
-		if (!strings.ContainsABBA(b) && (strings.ContainsABBA(a) || strings.ContainsABBA(c))) {
-			result++
+		innerABBA := false
+
+		for _, i := range inner {
+			if ContainsABBA(i) {
+				innerABBA = true
+				break
+			}
+		}
+
+		if innerABBA {
+			continue
+		}
+
+		for _, o := range outer {
+			if ContainsABBA(o) {
+				result++
+				break
+			}
 		}
 	}
+}
+
+func parseInput(line string) (outer, inner []string) {
+	current := ""
+
+	for _, rn := range line {
+		switch char := string(rn); char {
+		case "[":
+			outer = append(outer, current)
+			current = ""
+		case "]":
+			inner = append(inner, current)
+			current = ""
+		default:
+			current += char
+		}
+	}
+
+	outer = append(outer, current)
+
+	return outer, inner
 }
 
 // Solve part 2
 func Solve2() {
 
-}
+	for _, line := range lines {
+		outer, inner := parseInput(line)
 
-func getStringsOfLine(line string) (left,inner,right string) {
-	left = ""
-	inner = ""
-	right = ""
-	innerMode := false
-	rightMode := false
+	outer:
+		for _, o := range outer {
+			pairs, ok := ContainsABA(o)
 
-	for _,r := range line {
-		if r == '[' {
-			innerMode = true
-			continue
-		}
-
-		if r == ']' {
-			innerMode = false
-			rightMode = true
-			continue
-		}
-
-		if innerMode {
-			inner += string(r)
-		} else if rightMode {
-			right += string(r)
-		} else {
-			left += string(r)
+			if ok {
+				for _, i := range inner {
+					for _, pair := range pairs {
+						if ContainsBAB(i, pair[0], pair[1]) {
+							result++
+							continue outer
+						}
+					}
+				}
+			}
 		}
 	}
+}
 
-	return left,inner,right
+func ContainsABBA(str string) bool {
+	for i := 3; i < len(str); i++ {
+		// match outsides, match insides, ensure insides and outsides are different
+		if str[i-3] == str[i] && str[i-2] == str[i-1] && str[i] != str[i-1] {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsABA(str string) (pairs [][]string, ok bool) {
+	ok = false
+	for i := 2; i < len(str); i++ {
+		// match outsides, match insides, ensure insides and outsides are different
+		if str[i-2] == str[i] && str[i] != str[i-1] {
+			pairs = append(pairs, []string{string(str[i]), string(str[i-1])})
+			ok = true
+		}
+	}
+	return pairs, ok
+}
+
+func ContainsBAB(str, a, b string) bool {
+	for i := 2; i < len(str); i++ {
+		// match outsides, match insides, ensure insides and outsides are different
+		inner := str[i-2 : i+1]
+		if inner == b+a+b {
+			return true
+		}
+	}
+	return false
 }
 
 func readInput() {
--- pkg/strings/palindrom.go
+++ pkg/strings/palindrom.go
@@ -1,16 +1,5 @@
 package strings
 
-// check if a string is a palindrome and A != B
-func ContainsABBA(s string) bool {
-	for i := 0; i < len(s)-3; i++ {
-		if IsPalindrome(s[i : i+4]) && s[i] != s[i+1] {
-			return true
-		}
-	}
-
-	return false
-}
-
 func IsPalindrome(s string) bool {
 	for i := 0; i < len(s)/2; i++ {
 		if s[i] != s[len(s)-(1+i)] {
