65ce41fe327784531fd1b794c5e54533b65d4b02
Thomas Döring <thomas.doering@check24.de>
2023-12-01 08:06:19 +0100

    2023/1

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -11,58 +11,45 @@ import (
 )
 
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
 func println(f string) { fmt.Fprintln(writer, f) }
-func printf(f string) { fmt.Fprintf(writer, f) }
+func printf(f string)  { fmt.Fprintf(writer, f) }
 
 var lines []string
 var result int
 var year = 2023
 var day = 1
 
-
 func main() {
-  	// STDOUT MUST BE FLUSHED MANUALLY!!!
-  	defer writer.Flush()
+	// STDOUT MUST BE FLUSHED MANUALLY!!!
+	defer writer.Flush()
 
-  	readInput()
-
-	result = 0
-  	Solve1()
-  	println("1:" + strconv.Itoa(result))
+	readInput()
 
 	result = 0
-  	Solve2()
-  	println("2:" + strconv.Itoa(result))
+	Solve1()
+	println("1:" + strconv.Itoa(result))
+
+	result = 0
+	Solve2()
+	println("2:" + strconv.Itoa(result))
 }
 
 // Solve part 1
 func Solve1() {
-	for _, line := range lines {
-		number := ""
-		for _, c := range line {
-			// check if c is 0 - 9
-			if c >= 48 && c <= 57 {
-				number += string(c)
-			}
-		}
-
-		if number != "" {
-			// get only the first and last rune
-			number = string(number[0]) + string(number[len(number)-1])
-
-			n, err := strconv.Atoi(number)
-			if err != nil {
-				log.Fatal(err)
-			}
-			result += n
-		}
-	}
+	solve(false)
 }
 
 // Solve part 2
 func Solve2() {
+	solve(true)
+}
+
+func solve(two bool) {
 	for _, line := range lines {
-		line = replaceNumberWords(line)
+		if two {
+			line = replaceNumberWords(line)
+		}
 		number := ""
 		for _, c := range line {
 			// check if c is 0 - 9
@@ -74,7 +61,6 @@ func Solve2() {
 		if number != "" {
 			// get only the first and last rune
 			number = string(number[0]) + string(number[len(number)-1])
-			println(number)
 
 			n, err := strconv.Atoi(number)
 			if err != nil {
@@ -86,19 +72,33 @@ func Solve2() {
 }
 
 func replaceNumberWords(s string) string {
-    numberWords := map[string]string{
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
+	// map of number words to their number
+	// e.g. "one" -> "1e"
+	// e -> for overlapping words
+	numberWords := map[string]string{
+		"zero":  "0o",
+		"one":   "1e",
+		"two":   "2o",
+		"three": "3e",
+		"four":  "4r",
+		"five":  "5e",
+		"six":   "6x",
+		"seven": "7n",
+		"eight": "8t",
+		"nine":  "9e",
+	}
 
-	  return s
+	for i := 0; i < len(s); i++ {
+		for k, v := range numberWords {
+			if s[i] == k[0] {
+				if i+len(k) <= len(s) && s[i:i+len(k)] == k {
+					s = s[:i] + v + s[i+len(k):]
+				}
+			}
+		}
+	}
+
+	return s
 }
 
 func readInput() {
