f0e8e9faea37be0400676fbeef5c2a53771e36a6
MarluxGitHub <marlux.td@gmail.com>
2024-12-03 19:54:25 +0100

    2015/12

--- 2015/12/main.go
+++ 2015/12/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"MarluxGitHub/adventOfCode/pkg/datastructures"
 	"bufio"
 	"fmt"
 	"log"
@@ -38,101 +39,109 @@ func main() {
 
 // Solve part 1
 func Solve1() {
+	result = getSumOfAllNumbersInString(lines[0])
+}
+
+// Solve part 2
+func Solve2() {
+	reg := "\"red\""
+	regex := regexp.MustCompile(reg)
+
+	for {
+		i := regex.FindStringIndex(lines[0])
+
+		if i == nil {
+			break
+		}
+
+		stack := datastructures.NewStack()
+		left := -1
+		right := -1
+
+		isCurlyBracket := false
+
+	left:
+		for j := i[0] - 1; j >= 0; j-- {
+			switch lines[0][j] {
+			case '{':
+				if stack.IsEmpty() {
+					left = j
+					isCurlyBracket = true
+					break left
+				} else {
+					stack.Pop()
+				}
+			case '}':
+				stack.Push('}')
+			case '[':
+				if stack.IsEmpty() {
+					left = j
+					break left
+				} else {
+					stack.Pop()
+				}
+			case ']':
+				stack.Push(']')
+			}
+		}
+
+	right:
+		for j := i[1]; j < len(lines[0]); j++ {
+			switch lines[0][j] {
+			case '{':
+				stack.Push('}')
+			case '}':
+				if stack.IsEmpty() {
+					isCurlyBracket = true
+					right = j
+					break right
+				}
+				stack.Pop()
+			case '[':
+				stack.Push(']')
+			case ']':
+				if stack.IsEmpty() {
+					right = j
+					break right
+				}
+				stack.Pop()
+			}
+		}
+
+		if left == -1 || right == -1 {
+			continue
+		}
+
+		if isCurlyBracket {
+			lines[0] = lines[0][:left] + lines[0][right+1:]
+		} else {
+			substring := lines[0][left : right+1]
+			sum := getSumOfAllNumbersInString(substring)
+			lines[0] = lines[0][:left] + "[" + strconv.Itoa(sum) + "]" + lines[0][right+1:]
+		}
+	}
+
+	// print lines[0] in test.json
+	os.WriteFile("test.json", []byte(lines[0]), 0644)
+
+	result = getSumOfAllNumbersInString(lines[0])
+}
+
+func getSumOfAllNumbersInString(s string) int {
 	// get All Numbers in the String
 	reg := "(-?\\d+)"
 	regexp := regexp.MustCompile(reg)
 
 	// get all the matches
-	matches := regexp.FindAllString(lines[0], -1)
+	matches := regexp.FindAllString(s, -1)
 
+	sum := 0
 	for _, match := range matches {
 		num, _ := strconv.Atoi(match)
-		result += num
-	}
-}
-
-// Solve part 2
-func Solve2() {
-	// get all Locations of the String Red
-
-	for {
-		// find the first occurence of the word red
-		redIndex := -1
-		for i, c := range lines[0] {
-			if c == '"' && i < len(lines[0])-2 && lines[0][i+1] == 'r' && lines[0][i+2] == 'e' && lines[0][i+3] == 'd' && lines[0][i+4] == '"' {
-				redIndex = i
-				break
-			}
-		}
-
-		// if no red is found, break
-		if redIndex == -1 {
-			break
-		}
-
-		// find the first { and }
-		openIndex := -1
-		closeIndex := -1
-		bracket := false
-		for i := redIndex - 5; i >= 0; i-- {
-			if lines[0][i] == '{' {
-				openIndex = i
-				break
-			}
-
-			if lines[0][i] == ']' {
-				bracket = true
-			}
-
-			if lines[0][i] == '[' {
-				if bracket {
-					bracket = false
-				} else {
-					break
-				}
-			}
-		}
-
-		bracket = false
-		for i := redIndex; i < len(lines[0]); i++ {
-			if lines[0][i] == '}' {
-				closeIndex = i
-				break
-			}
-
-			if lines[0][i] == '[' {
-				bracket = true
-			}
-
-			if lines[0][i] == ']' {
-				if bracket {
-					bracket = false
-				} else {
-					break
-				}
-			}
-		}
-
-		// remove the string from the openIndex to the closeIndex
-		if openIndex != -1 && closeIndex != -1 {
-			lines[0] = lines[0][:openIndex] + lines[0][closeIndex+1:]
-		} else {
-			break
-		}
+		sum += num
 	}
 
-	// get All Numbers in the String
-	reg := "(-?\\d+)"
-	regex := regexp.MustCompile(reg)
-
-	// get all the matches
-	matches := regex.FindAllString(lines[0], -1)
-
-	for _, match := range matches {
-		num, _ := strconv.Atoi(match)
-		result += num
-	}
-
+	return sum
 }
 
 func readInput() {
